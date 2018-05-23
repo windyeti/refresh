97a8ce195482c7ddc475749852daccd6e3ce5a42
Егор Тиханин <egortihanin@Egors-Mac-Pro.local>
2018-05-23 15:35:31 +0300

    ready simple store-redux

--- src/App.js
+++ src/App.js
@@ -1,9 +1,29 @@
 import React from 'react';
+// import { Link, Router, Route } from 'react-router';
+
+import { createStore } from 'redux';
+
 import Dropdown from 'containers/dropdown';
 import Header from 'containers/header';
 import RegistrationForm from 'containers/registrationForm';
 
 import './App.css'
+
+function reducerPlaylist(state = [], action) {
+    if(action.type === 'ADD_TRACK') {
+        return [
+            ...state, action.payload
+        ]
+    }
+}
+
+const store = createStore(reducerPlaylist);
+
+store.subscribe(() => {
+    console.log('store', store.getState());
+});
+
+store.dispatch({ type : 'ADD_TRACK', payload : 'Jora song'});
 
 class App extends React.Component {
   render() {
--- src/index.js
+++ src/index.js
@@ -4,5 +4,8 @@ import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App name={'Куку!'}/>, document.getElementById('root'));
+ReactDOM.render(
+    <App />,
+    document.getElementById('root'));
+
 registerServiceWorker();
